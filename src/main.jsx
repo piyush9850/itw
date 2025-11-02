17b58b1ffea8e04b2ce8cdb17d9ff342327fa3e1
sparsh912 <sparshmaheshwari0908@gmail.com>
2025-11-02 17:15:38 +0530

    Some changes in routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
-import Layout from './layout.jsx'
+import Layout from "./layout.jsx";
 
-import Contact from './pages/Contact.jsx'
-import Home from './pages/Home.jsx'
-import NotFound from './pages/NotFound.jsx'
-import Project from './pages/Project.jsx'
-import Skill from './pages/Skill.jsx'
+import Contact from "./pages/Contact.jsx";
+import Home from "./pages/Home.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import Project from "./pages/Project.jsx";
+import Skill from "./pages/Skill.jsx";
 
-const router =createBrowserRouter(
+const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<Layout />}>
-      <Route path='' element={<Home />} />
-      <Route path='contact' element={<Contact />} />
-      <Route path='project' element={<Project />} />
-      <Route path='skill' element={<Skill />} />
+    <>
+      {/* ✅ Routes with layout */}
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="project" element={<Project />} />
+        <Route path="skill" element={<Skill />} />
+      </Route>
 
-          {/* 👇 This catches any undefined route */}
+      {/* 🚫 NotFound route without layout */}
       <Route path="*" element={<NotFound />} />
-    </Route>
+    </>
   )
-)
+);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
